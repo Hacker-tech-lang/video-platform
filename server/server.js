2fab7a41486e1158d540606e6d19df37f17d0aca
Hacker-tech-lang <compasivochristianpaul22@gmail.com>
2024-09-21 18:38:55 +0800

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,7 @@
+document.querySelector('.like-btn').addEventListener('click', () => {
+    alert('Liked the video!');
+});
+
+document.querySelector('.subscribe-btn').addEventListener('click', () => {
+    alert('Subscribed!');
+});
